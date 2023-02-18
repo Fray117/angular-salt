963f9248c2de7deb1d7d34b65eee85f7a51c77a3
Fray Dalton <fray117@exploiter.id>
2023-02-18 14:41:57 +0700

    revert

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import 'zone.js/dist/zone'
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+import { HttpClientModule } from '@angular/common/http'
+import { ContainerComponent } from './container/container.component'
+import { NavbarComponent } from './navbar/navbar.component'
+import { FooterComponent } from './footer/footer.component'
+import { VideosListComponent } from './videos-list/videos-list.component'
+import { ActivityListComponent } from './activity-list/activity-list.component'
+import { PeoplesListComponent } from './peoples-list/peoples-list.component'
+import { DocumentsListComponent } from './documents-list/documents-list.component'
+import { ChannelsListComponent } from './channels-list/channels-list.component'
+
+@NgModule({
+	declarations: [
+		ContainerComponent,
+		NavbarComponent,
+		FooterComponent,
+		VideosListComponent,
+		ActivityListComponent,
+		PeoplesListComponent,
+		DocumentsListComponent,
+		ChannelsListComponent
+	],
+	imports: [
+		BrowserModule,
+		HttpClientModule
+	],
+	providers: [],
+	bootstrap: [ContainerComponent]
+
+})
+export class AppModule {
+	name = 'Angular';
+}
